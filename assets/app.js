19e4230f597a1d3a5df3252c917dfdffa9110c88
kennethtxytqw <kennethtxytqw@gmail.com>
2016-06-23 13:46:15 +0800

    Done improving UI

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,24 @@
 var main = function(){
 
-	$('.btn').click(function(){
-		$('.container').show();
-		$('.btn').hide();
+	$('button').click(function(event) {
+		event.preventDefault(); 
+	});
 
+	$('button[name="compose_now_btn"]').click(function(){
+		$('div[name="uploader"]').show();
+		$('button[name="compose_now_btn"]').hide();
+	});
+
+	$('button[name="upload_btn"]').click(function(){
+		$('#upload_files').click();
+	});
+
+	$('button[name="upload"]').click(function(){
+		alert('Files uploaded!');
+	});
+
+	$('.glyphicon-remove').click(function(){
+		alert('File removed!');
 	});
 };
 
