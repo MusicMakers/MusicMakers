2cf26d882cb559674080b4ddba48c32c616a6b8c
l0g1cal <yongjiawern@gmail.com>
2016-07-04 17:15:16 +0800

    Add files via upload

--- Music Algorithm/MidiMusicCreator.java
+++ Music Algorithm/MidiMusicCreator.java
@@ -3,8 +3,8 @@ package jMusicTest;
 // This second attempt is a success! However, its input and output is limited to midi files
 
 import java.util.*;
-
-import java.io.File;
+import jm.JMC;
+import java.io.*;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.MidiSystem;
@@ -18,67 +18,82 @@ import jm.music.data.Phrase;
 import jm.music.data.Score;
 import jm.util.Write;
 
-public class MidiMusicCreator {
+public class MidiMusicCreator implements JMC {
 	public static final int NOTE_ON = 0x90;
     public static final int NOTE_OFF = 0x80;
 	private int m_order = 0;
 	private HashMap<ArrayList<Integer>, ArrayList<Integer>> keyMap = new HashMap<ArrayList<Integer>, ArrayList<Integer>>(); 
 	private HashMap<ArrayList<Double>, ArrayList<Double>> rhythmMap = new HashMap<ArrayList<Double>, ArrayList<Double>>(); 
+	private double[] countList = {C, HNT, CD, CDD, M, DM, DDM, SEMIBREVE, DEMI_SEMI_QUAVER_TRIPLET,
+			DEMI_SEMI_QUAVER, SEMI_QUAVER_TRIPLET, SQ, QT, DOTTED_SEMI_QUAVER, Q, CT, DEN, DDEN};
 	private ArrayList<Integer> keyList = new ArrayList<Integer> ();
 	private ArrayList<Integer> timeList = new ArrayList<Integer> ();
 	private ArrayList<Double> rhythmList = new ArrayList<Double> ();
 	private ArrayList<Integer> tempKeyList = new ArrayList<Integer> ();
 	private ArrayList<Double> tempRhythmList = new ArrayList<Double> ();
-	private count c = new count();
-	private HashMap<Integer, Double> countMap = c.getCount();
+	private HashMap<Integer, Double> countMap;
+	
 	int previousTime = 0;
 	int currentTime = 0;
 	
 	MidiMusicCreator(String midiFile, int order) throws Exception {
 		Sequence sequence = MidiSystem.getSequence(new File(midiFile));
 		m_order = order;
-		process(sequence);
+		count c = new count(sequence.getResolution());
+		countMap = c.getCount();
+		process(sequence);		
 	}
 	
 	public void process(Sequence s) throws Exception{
-		for (Track track: s.getTracks()){
+		// -1 because the first track is not useful (at least at the moment)
+		int numTracks = s.getTracks().length;
+			Track track = s.getTracks()[1];
 			for(int i=0; i<track.size(); i++){
 				MidiEvent event = track.get(i);
 				MidiMessage message = event.getMessage();
 				if (message instanceof ShortMessage){
 					// convert to short message
 					ShortMessage sm = (ShortMessage) message;
-					if (sm.getCommand() == NOTE_ON || sm.getCommand() == NOTE_OFF){
+					if (sm.getCommand() == NOTE_ON){
 						int key = sm.getData1();
-                     int velocity = sm.getData2();
-                     
-                     // velocity is 0 implies note fades right away
-                     if (velocity != 0) {
-                     	previousTime = (int) event.getTick();
-                     	keyList.add(key);    	
-//                     	System.out.println("Key: " +key);
-                     }
-                     else {
-                     	currentTime = (int) event.getTick();
-                     	timeList.add(currentTime - previousTime);
-                     	System.out.println("Time: " + (currentTime - previousTime));
-                     }
+						int velocity = sm.getData2();
+	                 
+						// velocity is 0 implies note fades right away
+						if (velocity != 0) {
+							currentTime = (int) event.getTick();
+							if (currentTime != 0) {
+								keyList.add(key);
+//								System.out.println("current tick: " + currentTime);
+								timeList.add(currentTime - previousTime);
+//								System.out.println("Time: " + (currentTime - previousTime));
+							}
+							
+//	                     	System.out.println("Key: " +key);
+						}
+//						else {
+//							currentTime = (int) event.getTick();
+//							System.out.println("current tick: " + currentTime);
+//							timeList.add(currentTime - previousTime);
+//							System.out.println("Time: " + (currentTime - previousTime));
+//						}
 					}
+					previousTime = currentTime;
 				}
 			}
-		}
-		// converting integer values to the actual rhythm values suing the 'count' class created
-		for (int value: timeList){
-			System.out.println("Value: " + value);
-			if (!countMap.containsKey(value)){
-				throw new Exception("There is no such rhythmic value");
+			// converting integer values to the actual rhythm values suing the 'count' class created
+			for (int value: timeList){
+//				System.out.println("Value: " + value);
+				if (!countMap.containsKey(value)){
+//					throw new Exception("There is no such rhythmic value "+ value);
+					rhythmList.add(countList[new Random().nextInt(countList.length)]);
+				}
+				else {
+//					System.out.println("Rhythm: " + countMap.get(value));
+					rhythmList.add(countMap.get(value));
+				}
 			}
-			else {
-				rhythmList.add(countMap.get(value));
-			}
+			createHashTable(keyList, rhythmList, m_order);
 		}
-		createHashTable(keyList, rhythmList, m_order);
-	}
 	
 	public void createHashTable(ArrayList<Integer> klist, ArrayList<Double> rlist, int order) {
 		int i = 0;
@@ -93,8 +108,8 @@ public class MidiMusicCreator {
 					nextRhythm = rlist.get(order+i);
 				}
 			}
-			System.out.println("Temp Key: " + tempKeyList.toString());
-			System.out.println("Rhythm Key: " + tempRhythmList.toString());
+//			System.out.println("Temp Key: " + tempKeyList.toString());
+//			System.out.println("Rhythm Key: " + tempRhythmList.toString());
 			
 			if (!keyMap.containsKey(tempKeyList)){
 				ArrayList<Integer> a = new ArrayList<Integer> ();
@@ -154,22 +169,22 @@ public class MidiMusicCreator {
 			if (keyMap.containsKey(tempKeyList)){
 				ArrayList<Integer> a = keyMap.get(tempKeyList);
 				nextNote = a.get(new Random().nextInt(a.size()));
-				System.out.println("Have");
+//				System.out.println("Have");
 			}
 			// if not
 			else {
 				nextNote = keyList.get(new Random().nextInt(keyList.size()));
-				System.out.println("Don't have");
+//				System.out.println("Don't have");
 			}
 			if (rhythmMap.containsKey(tempRhythmList)){
 				ArrayList<Double> b = rhythmMap.get(tempRhythmList);
 				nextRhythm = b.get(new Random().nextInt(b.size()));
-				System.out.println("Have");
+//				System.out.println("Have");
 			}
 			// if not
 			else {
 				nextRhythm = rhythmList.get(new Random().nextInt(rhythmList.size()));
-				System.out.println("Don't have");
+//				System.out.println("Don't have");
 			}
 //			note.setPitch(nextNote);
 //			phrase.addNote(note);
@@ -187,14 +202,14 @@ public class MidiMusicCreator {
 		scr.addPart(part);
 		scr.setTempo(tempo);
 		Write.midi(scr, "Second Attempt.mid");
-		
 	}
 	
 	public static void main(String[] args) throws Exception {		
-		// filename, Markov Order	
-		MidiMusicCreator t = new MidiMusicCreator("rowboat.mid",1);
-		// numNotes, tempo
-		t.generateMusic(30,100);
+		// filename, Markov Order
+		
+		MidiMusicCreator t = new MidiMusicCreator("brahms-lullaby-wiegenlied.mid",2);
+//		 numNotes, tempo
+		t.generateMusic(100,100);
 		
  }
 }
